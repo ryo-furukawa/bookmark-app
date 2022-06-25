abb9d61fcc0414842b02004db66f90fb5d89b0b4
ryo-furukawa <furu3548@gmail.com>
2022-06-26 06:43:01 +0900

    add: Laravel9 + Vue3 + TypeScript 環境構築

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+onMounted(() => {
+    console.log("on mount")
+})
+</script>
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
